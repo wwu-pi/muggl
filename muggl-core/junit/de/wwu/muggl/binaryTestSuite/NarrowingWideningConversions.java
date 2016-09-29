70949badc1ab7d5286830e19dfe7c3d5fb0e509f
Max Schulze <max.schulze@uni-muenster.de>
2016-09-29 16:22:02 +0200

    Tests for narrowingWidening Conversions

---
+++ muggl-core/junit/de/wwu/muggl/binaryTestSuite/NarrowingWideningConversions.java
@@ -0,0 +1,102 @@
+package de.wwu.muggl.binaryTestSuite;
+
+/**
+ * This class shall test the §2.11.4 Type Conversion Instructions
+ * 
+ * @author Max Schulze
+ *
+ */
+public class NarrowingWideningConversions {
+	// NARROWING
+
+	public static byte i2b() {
+		int test = 4;
+		return (byte) test;
+	}
+
+	public static char i2c() {
+		int test = 49;
+		return (char) test;
+	}
+
+	public static short i2s() {
+		int test = 4;
+		return (short) test;
+	}
+
+	public static int l2i() {
+		long test = 3245;
+		return (int) test;
+	}
+
+	public static int f2i() {
+		float test = 3L;
+		return (int) test;
+	}
+
+	public static long f2l() {
+		float test = 3L;
+		return (long) test;
+	}
+
+	public static int d2i() {
+		double test = 3.9999999999999;
+		return (int) test;
+	}
+
+	public static long d2l() {
+		double test = 3.9999999999999;
+		return (long) test;
+	}
+
+	public static float d2f() {
+		double test = 3.9999999999999;
+		return (float) test;
+	}
+
+	// WIDENING
+	public static long i2l() {
+		int test = 32550;
+		return (long) test;
+	}
+
+	public static float i2f() {
+		int test = 32445;
+		return (float) test;
+	}
+
+	public static double i2d() {
+		int test = 32443;
+		return (double) test;
+	}
+
+	public static float l2f() {
+		long test = 48888;
+		return (float) test;
+	}
+
+	public static double l2d() {
+		long test = 499999;
+		return (double) test;
+	}
+
+	public static double f2d() {
+		float test = 444444;
+		return (double) test;
+	}
+
+	// implicit ones, via Integer. No full coverage, just spot checks
+	public static byte l2b() {
+		long test = -7;
+		return (byte) test;
+	}
+
+	public static short d2s() {
+		double test = 3.49999999999999;
+		return (short) test;
+	}
+
+	public static void main(String[] args) {
+		// FIXME mxs: develop tests for narrowing widening
+	}
+}
---
+++ muggl-core/junit/de/wwu/muggl/test/real/instructions/TestNarrowingWideningInstructions.java
@@ -0,0 +1,248 @@
+package de.wwu.muggl.test.real.instructions;
+
+import static org.junit.Assert.*;
+
+import java.lang.invoke.MethodType;
+
+import org.apache.log4j.Level;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.wwu.muggl.configuration.Globals;
+import de.wwu.muggl.instructions.InvalidInstructionInitialisationException;
+import de.wwu.muggl.test.real.vm.TestVMNormalMethodRunnerHelper;
+import de.wwu.muggl.vm.classfile.ClassFileException;
+import de.wwu.muggl.vm.execution.ConversionException;
+import de.wwu.muggl.vm.initialization.InitializationException;
+import de.wwu.muggl.vm.loading.MugglClassLoader;
+
+/**
+ * This class shall test the §2.11.4 Type Conversion Instructions.
+ * 
+ * See also Table 2.11.1-A "Type support in the Java Virtual Machine instruction set"
+ * 
+ * @author Max Schulze
+ *
+ */
+public class TestNarrowingWideningInstructions {
+	MugglClassLoader classLoader;
+
+	// how much epsilon is permitted in double comparisons?
+	private static final double epsilon = 1e-10;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Globals.getInst().changeLogLevel(Level.ERROR);
+		Globals.getInst().parserLogger.setLevel(Level.ERROR);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		classLoader = new MugglClassLoader(new String[] { "./", "./junit-res/" });
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void test_instr_d2f() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((float) 4,
+				(float) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "d2f",
+						MethodType.methodType(float.class).toMethodDescriptorString(), null),
+				epsilon);
+
+	}
+
+	@Test
+	public final void test_instr_d2i() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((int) 3,
+				(int) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "d2i",
+						MethodType.methodType(int.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_instr_d2l() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((long) 3,
+				(long) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "d2l",
+						MethodType.methodType(long.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_impl_d2s() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((short) 3,
+				(short) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "d2s",
+						MethodType.methodType(short.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_instr_f2d() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((double) 444444,
+				(double) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "f2d",
+						MethodType.methodType(double.class).toMethodDescriptorString(), null),
+				epsilon);
+
+	}
+
+	@Test
+	public final void test_instr_f2i() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((int) 3,
+				(int) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "f2i",
+						MethodType.methodType(int.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_instr_f2l() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((long) 3,
+				(long) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "f2l",
+						MethodType.methodType(long.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_instr_i2b() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((byte) 4,
+				(byte) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "i2b",
+						MethodType.methodType(byte.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_instr_i2c() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((char) 49,
+				(char) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "i2c",
+						MethodType.methodType(char.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_instr_i2d() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((double) 32443,
+				(double) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "i2d",
+						MethodType.methodType(double.class).toMethodDescriptorString(), null),
+				epsilon);
+
+	}
+
+	@Test
+	public final void test_instr_i2f() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((float) 32445,
+				(float) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "i2f",
+						MethodType.methodType(float.class).toMethodDescriptorString(), null),
+				epsilon);
+
+	}
+
+	@Test
+	public final void test_instr_i2l() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((long) 32550,
+				(long) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "i2l",
+						MethodType.methodType(long.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_instr_i2s() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((short) 4,
+				(short) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "i2s",
+						MethodType.methodType(short.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_impl_l2b() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((byte) -7,
+				(byte) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "l2b",
+						MethodType.methodType(byte.class).toMethodDescriptorString(), null));
+
+	}
+
+	@Test
+	public final void test_instr_l2d() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((double) 499999,
+				(double) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "l2d",
+						MethodType.methodType(double.class).toMethodDescriptorString(), null),
+				epsilon);
+
+	}
+
+	@Test
+	public final void test_instr_l2f() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((float) 48888,
+				(float) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "l2f",
+						MethodType.methodType(float.class).toMethodDescriptorString(), null),
+				epsilon);
+
+	}
+
+	@Test
+	public final void test_instr_l2i() throws ClassFileException, InitializationException, InterruptedException,
+			InvalidInstructionInitialisationException, ConversionException {
+
+		assertEquals((int) 3245,
+				(int) TestVMNormalMethodRunnerHelper.runMethod(classLoader,
+						de.wwu.muggl.binaryTestSuite.NarrowingWideningConversions.class.getCanonicalName(), "l2i",
+						MethodType.methodType(int.class).toMethodDescriptorString(), null));
+
+	}
+
+}
