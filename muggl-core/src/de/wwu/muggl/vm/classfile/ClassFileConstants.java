2d204842998da1e856590539b7c5157302fdf618
Max Schulze <max.schulze@uni-muenster.de>
2016-10-24 13:14:39 +0200

    prepare ClassFileConstants separate File

---
+++ muggl-core/src/de/wwu/muggl/vm/classfile/ClassFileConstants.java
@@ -0,0 +1,13 @@
+package de.wwu.muggl.vm.classfile;
+
+public final class ClassFileConstants {
+
+	// Constants for the constant_pool.
+	// openjdk equivalence in openjdk/jdk/src/share/javavm/export/classfile_constants.h
+	// and openjdk/hotspot/src/share/vm/prims/jvm.h
+
+	// FIXME mxs: would make sense to move the ClassFile Constants here to have a shorter file over there.
+
+	/* CONSTANT_MethodHandle types are defined in de.wwu.muggl.vm.classfile.structures.constants.ConstantMethodHandle */
+
+}
