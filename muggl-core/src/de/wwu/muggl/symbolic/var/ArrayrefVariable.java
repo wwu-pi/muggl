224cfe830d56eb801ab2eb61c0ee5a4826f5ed3e
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-03-20 12:56:06 +0100

    add constraints on index and size of array ref when adding new elements

--- muggl-core/src/de/wwu/muggl/symbolic/var/ArrayrefVariable.java
+++ muggl-core/src/de/wwu/muggl/symbolic/var/ArrayrefVariable.java
@@ -3,10 +3,15 @@ package de.wwu.muggl.symbolic.var;
 import java.io.PrintStream;
 
 import de.wwu.muggl.solvers.Solution;
+import de.wwu.muggl.solvers.exceptions.SolverUnableToDecideException;
+import de.wwu.muggl.solvers.exceptions.TimeoutException;
 import de.wwu.muggl.solvers.expressions.Expression;
 import de.wwu.muggl.solvers.expressions.GreaterOrEqual;
+import de.wwu.muggl.solvers.expressions.Max;
 import de.wwu.muggl.solvers.expressions.NumericConstant;
+import de.wwu.muggl.solvers.expressions.NumericEqual;
 import de.wwu.muggl.solvers.expressions.NumericVariable;
+import de.wwu.muggl.solvers.expressions.Sum;
 import de.wwu.muggl.solvers.expressions.TypeCheckException;
 import de.wwu.muggl.solvers.expressions.ref.meta.ReferenceVariable;
 import de.wwu.muggl.solvers.solver.constraints.Assignment;
@@ -33,6 +38,11 @@ public class ArrayrefVariable extends Arrayref implements ReferenceVariable, Ref
 	protected NumericVariable length;
 	
 	/**
+	 * Internal variable for the maximum index. Constraint: maxIndex+1 = length
+	 */
+	protected NumericVariable maxIndex;
+	
+	/**
 	 * The name of this variable.
 	 */
 	protected String name;
@@ -57,25 +67,39 @@ public class ArrayrefVariable extends Arrayref implements ReferenceVariable, Ref
 	 * @param name the name of the variable.
 	 * @param arrayType the type of the array
 	 * @param vm the virtual machine
+	 * @throws SolverUnableToDecideException 
+	 * @throws TimeoutException 
 	 */
-	public ArrayrefVariable(String name, ReferenceValue arrayType, SymbolicVirtualMachine vm) {
+	public ArrayrefVariable(String name, ReferenceValue arrayType, SymbolicVirtualMachine vm) throws TimeoutException, SolverUnableToDecideException {
 		super(arrayType, 0);
 		this.vm = vm;
 		this.name = name;
 		this.arrayType = arrayType;
 		this.symbolicArray = new SymbolicArray();
 		this.length = new NumericVariable(name+".length", Expression.INT);
+		this.maxIndex = new NumericVariable(name+".maxIndex", Expression.INT);
 		this.isNull = new NumericVariable(name+".isNull", Expression.BOOLEAN);
 		this.vm.getSolverManager().addConstraint(GreaterOrEqual.newInstance(length, NumericConstant.getZero(Expression.INT)));
+		this.vm.getSolverManager().addConstraint(GreaterOrEqual.newInstance(maxIndex, NumericConstant.getZero(Expression.INT)));
+		this.vm.getSolverManager().addConstraint(NumericEqual.newInstance(length, Sum.newInstance(this.maxIndex, NumericConstant.getOne(Expression.INT))));
+		if(!this.vm.getSolverManager().hasSolution()) {
+			throw new RuntimeException("Cannot generate array reference, no solution possible!");
+		}
 	}
 	
 	
 	public void addElementAt(int index, Object element) {
+		if(index < 0) {
+			throw new RuntimeException("Sorry, but an index must be >= 0");
+		}
+		this.vm.getSolverManager().addConstraint(GreaterOrEqual.newInstance(this.maxIndex, NumericConstant.getInstance(index, Expression.INT)));
 		this.symbolicArray.addElementAt(index, element);
 	}
 	
 	public void addElementAt(NumericVariable index, Object element) {
 		this.symbolicArray.addElementAt(index, element);
+		this.vm.getSolverManager().addConstraint(GreaterOrEqual.newInstance(index, NumericConstant.getZero(Expression.INT)));
+		this.vm.getSolverManager().addConstraint(Max.newInstance(this.symbolicArray.getSymElementMap().keySet(), this.maxIndex));
 	}
 	
 	
