679e7bc8737dd22bf8ac4bebe89535e1c18dab52
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-01 16:41:23 +0200

    add implementation

--- muggl-core/src/de/wwu/muggl/javaee/invoke/impl/MugglEntityManagerFind.java
+++ muggl-core/src/de/wwu/muggl/javaee/invoke/impl/MugglEntityManagerFind.java
@@ -1,7 +1,18 @@
 package de.wwu.muggl.javaee.invoke.impl;
 
 import de.wwu.muggl.javaee.invoke.SpecialMethodInvocation;
+import de.wwu.muggl.javaee.invoke.SpecialMethodInvokeException;
+import de.wwu.muggl.javaee.jpa.MugglEntityManager;
+import de.wwu.muggl.solvers.expressions.IntConstant;
+import de.wwu.muggl.symbolic.var.ObjectrefVariable;
 import de.wwu.muggl.vm.Frame;
+import de.wwu.muggl.vm.classfile.ClassFile;
+import de.wwu.muggl.vm.classfile.ClassFileException;
+import de.wwu.muggl.vm.classfile.structures.Field;
+import de.wwu.muggl.vm.impl.symbolic.SymbolicVirtualMachine;
+import de.wwu.muggl.vm.initialization.Arrayref;
+import de.wwu.muggl.vm.initialization.InitializedClass;
+import de.wwu.muggl.vm.initialization.Objectref;
 
 /**
  * A special method invocation for:
@@ -12,9 +23,66 @@ import de.wwu.muggl.vm.Frame;
 public class MugglEntityManagerFind implements SpecialMethodInvocation {
 
 	@Override
-	public void execute(Frame frame) {
-		frame.getOperandStack().pop();
-		frame.getOperandStack().push(null);
+	public void execute(Frame frame, Object[] parameters) throws SpecialMethodInvokeException{
+		SymbolicVirtualMachine vm = (SymbolicVirtualMachine)frame.getVm();
+		Object mugglEntityManagerObj = frame.getOperandStack().pop();
+		if(!(mugglEntityManagerObj instanceof MugglEntityManager)) {
+			throw new SpecialMethodInvokeException("Expected object to be of type MugglEntityManager, but was: " + mugglEntityManagerObj);
+		}
+		
+		MugglEntityManager mem = (MugglEntityManager)mugglEntityManagerObj;
+		
+		String className = getClassName(parameters[1]);
+		
+		// check if the symbolic database already contains an entity of the same type with the same identifier
+//		if(mem.getEntityById(className, parameters[2])) {
+//			
+//		}
+		
+		ClassFile classFile = null;
+		try {
+			classFile = vm.getClassLoader().getClassAsClassFile(className);
+		} catch(ClassFileException e) {
+			throw new SpecialMethodInvokeException("Could not load required class file: " + className);
+		}
+		
+		String entityFindName = "FindResult@" + frame.getMethod().getName() + ":" + frame.getPc();
+		ObjectrefVariable entityFindVar = new ObjectrefVariable(entityFindName, new InitializedClass(classFile, vm), vm);
+				
+		frame.getOperandStack().push(entityFindVar);
 	}
 	
+	private String getClassName(Object object) throws SpecialMethodInvokeException {
+		if(object instanceof ObjectrefVariable) {
+			throw new SpecialMethodInvokeException("Objectref-Variables are not supported yet");
+		}
+		
+		if(object instanceof Objectref) {
+			Objectref objRef = (Objectref)object;
+			Field nameField = objRef.getInitializedClass().getClassFile().getFieldByNameAndDescriptor("name", "Ljava/lang/String;");
+			Objectref nameObjRef = (Objectref)objRef.getField(nameField); // Objectref of type String, now get the char array of it for the 'real' string values
+			Field valueField = nameObjRef.getInitializedClass().getClassFile().getFieldByNameAndDescriptor("value", "[C");
+			Arrayref valueArray = (Arrayref)nameObjRef.getField(valueField);
+			StringBuffer sb = new StringBuffer();
+			for(int i=0; i<valueArray.length; i++) {
+				Object c = valueArray.getElement(i);
+				if(c instanceof IntConstant)  {
+					IntConstant ic = (IntConstant)c;
+					int iVal = ic.getIntValue();
+					char charVal = (char)iVal;
+					sb.append(charVal);
+				} else {
+					throw new SpecialMethodInvokeException("String value char array of type: " + c + " not supported yet");
+				}
+			}
+			return sb.toString();
+		}
+		
+		throw new SpecialMethodInvokeException("Only object reference types supported yet");
+	}
+
+	@Override
+	public String toString() {
+		return "Special execution of EntityManager#find(Class,Object) via the MugglEntityManager";
+	}
 }
