3d4cc84812486f62df7fafbfd246a4db5ce77a7e
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-01 16:42:01 +0200

    add new exception

---
+++ muggl-core/src/de/wwu/muggl/javaee/invoke/SpecialMethodInvokeException.java
@@ -0,0 +1,12 @@
+package de.wwu.muggl.javaee.invoke;
+
+import de.wwu.muggl.configuration.MugglException;
+
+public class SpecialMethodInvokeException extends MugglException {
+
+	private static final long serialVersionUID = 1L;
+
+	public SpecialMethodInvokeException(String message) {
+		super(message);
+	}
+}
