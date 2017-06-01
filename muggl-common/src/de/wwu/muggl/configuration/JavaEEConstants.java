5d0efac8c85cb4f357b9ef519ef1995ad02ac7ef
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-01 10:19:23 +0200

    Add Java EE constants class

---
+++ muggl-common/src/de/wwu/muggl/configuration/JavaEEConstants.java
@@ -0,0 +1,19 @@
+package de.wwu.muggl.configuration;
+
+public final class JavaEEConstants {
+
+	/**
+	 * The Java String representation of the JPA annotation @javax.persistence.PersistenceContext.
+	 */
+	public static final String ANNOTATION_PERSISTENCE_CONTEXT = "Ljavax/persistence/PersistenceContext;";
+	
+	/**
+	 * The Java String representation of the JPA annotation @javax.inject.Inject.
+	 */
+	public static final String ANNOTATION_INJECT = "Ljavax/inject/Inject;";
+	
+	/**
+	 * The fully qualified name of the class of the JPA EntityManager.
+	 */
+	public static final String ENTITY_MANAGER_CLASS = "javax.persistence.EntityManager";
+}
