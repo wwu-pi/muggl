2638ccf93e2187b94cf00378dac28ef805575abc
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-01 11:19:05 +0200

    add @PostConstruct annotation constant

--- muggl-common/src/de/wwu/muggl/configuration/JavaEEConstants.java
+++ muggl-common/src/de/wwu/muggl/configuration/JavaEEConstants.java
@@ -13,6 +13,11 @@ public final class JavaEEConstants {
 	public static final String ANNOTATION_INJECT = "Ljavax/inject/Inject;";
 	
 	/**
+	 * The Java String representation of the JPA annotation @javax.annotation.PostConstruct.
+	 */
+	public static final String ANNOTATION_POST_CONSTRUCT = "Ljavax/annotation/PostConstruct;";
+	
+	/**
 	 * The fully qualified name of the class of the JPA EntityManager.
 	 */
 	public static final String ENTITY_MANAGER_CLASS = "javax.persistence.EntityManager";
