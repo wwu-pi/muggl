587b782cdccc70a62cc0369cc4ffe01b1caacf1b
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-01 16:40:59 +0200

    add parameters to method and exception

--- muggl-core/src/de/wwu/muggl/javaee/invoke/SpecialMethodInvocation.java
+++ muggl-core/src/de/wwu/muggl/javaee/invoke/SpecialMethodInvocation.java
@@ -11,6 +11,7 @@ public interface SpecialMethodInvocation {
 	/**
 	 * Execute special.
 	 * @param frame the current frame on which the method should be executed special.
+	 * @param parameters the parameters of the method to invoke
 	 */
-	public void execute(Frame frame);
+	public void execute(Frame frame, Object[] parameters) throws SpecialMethodInvokeException;
 }
