5d2fb57939e0b58f05af8a2a6613aa41542538b4
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-02 15:52:09 +0200

    add annotation constants

--- muggl-common/src/de/wwu/muggl/configuration/JavaEEConstants.java
+++ muggl-common/src/de/wwu/muggl/configuration/JavaEEConstants.java
@@ -18,6 +18,16 @@ public final class JavaEEConstants {
 	public static final String ANNOTATION_POST_CONSTRUCT = "Ljavax/annotation/PostConstruct;";
 	
 	/**
+	 * The Java String representation of the JPA annotation @javax.persistence.PersistenceContext.
+	 */
+	public static final String ANNOTATION_PERSISTENCE_ID = "Ljavax/persistence/Id;";
+	
+	/**
+	 * The Java String representation of the JPA annotation @javax.persistence.Entity.
+	 */
+	public static final String ANNOTATION_PERSISTENCE_ENTITY = "Ljavax/persistence/Entity;";
+	
+	/**
 	 * The fully qualified name of the class of the JPA EntityManager.
 	 */
 	public static final String ENTITY_MANAGER_CLASS = "javax.persistence.EntityManager";
