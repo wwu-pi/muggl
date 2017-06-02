d8edc69035e24ebf84fdb533f6d8dbe0dec78868
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-02 15:52:50 +0200

    add new constructor

--- muggl-core/src/de/wwu/muggl/javaee/invoke/SpecialMethodInvokeException.java
+++ muggl-core/src/de/wwu/muggl/javaee/invoke/SpecialMethodInvokeException.java
@@ -2,11 +2,20 @@ package de.wwu.muggl.javaee.invoke;
 
 import de.wwu.muggl.configuration.MugglException;
 
+/**
+ * An exception class for the special method invocations.
+ * 
+ * @author Andreas Fuchs
+ */
 public class SpecialMethodInvokeException extends MugglException {
 
 	private static final long serialVersionUID = 1L;
 
 	public SpecialMethodInvokeException(String message) {
 		super(message);
+	}
+	
+	public SpecialMethodInvokeException(String message, Throwable t) {
+		super(message, t);
 	}
 }
