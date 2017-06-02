beee20921e4220ffae6aaffe168e528459bbf4ea
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-02 15:53:27 +0200

    check if entity already exists in symbolic database

--- muggl-core/src/de/wwu/muggl/javaee/invoke/impl/MugglEntityManagerFind.java
+++ muggl-core/src/de/wwu/muggl/javaee/invoke/impl/MugglEntityManagerFind.java
@@ -3,6 +3,7 @@ package de.wwu.muggl.javaee.invoke.impl;
 import de.wwu.muggl.javaee.invoke.SpecialMethodInvocation;
 import de.wwu.muggl.javaee.invoke.SpecialMethodInvokeException;
 import de.wwu.muggl.javaee.jpa.MugglEntityManager;
+import de.wwu.muggl.javaee.jpa.SymbolicDatabaseException;
 import de.wwu.muggl.solvers.expressions.IntConstant;
 import de.wwu.muggl.symbolic.var.ObjectrefVariable;
 import de.wwu.muggl.vm.Frame;
@@ -23,7 +24,7 @@ import de.wwu.muggl.vm.initialization.Objectref;
 public class MugglEntityManagerFind implements SpecialMethodInvocation {
 
 	@Override
-	public void execute(Frame frame, Object[] parameters) throws SpecialMethodInvokeException{
+	public void execute(Frame frame, Object[] parameters) throws SpecialMethodInvokeException {
 		SymbolicVirtualMachine vm = (SymbolicVirtualMachine)frame.getVm();
 		Object mugglEntityManagerObj = frame.getOperandStack().pop();
 		if(!(mugglEntityManagerObj instanceof MugglEntityManager)) {
@@ -33,23 +34,36 @@ public class MugglEntityManagerFind implements SpecialMethodInvocation {
 		MugglEntityManager mem = (MugglEntityManager)mugglEntityManagerObj;
 		
 		String className = getClassName(parameters[1]);
+		Object idValue = parameters[2];
 		
-		// check if the symbolic database already contains an entity of the same type with the same identifier
-//		if(mem.getEntityById(className, parameters[2])) {
-//			
-//		}
-		
-		ClassFile classFile = null;
+		// check if the symbolic database already has an entity of the same type with the same identifier
 		try {
-			classFile = vm.getClassLoader().getClassAsClassFile(className);
-		} catch(ClassFileException e) {
-			throw new SpecialMethodInvokeException("Could not load required class file: " + className);
-		}
-		
-		String entityFindName = "FindResult@" + frame.getMethod().getName() + ":" + frame.getPc();
-		ObjectrefVariable entityFindVar = new ObjectrefVariable(entityFindName, new InitializedClass(classFile, vm), vm);
+			Objectref dbObjRef = mem.getDB().getEntityById(className, idValue);
+			if(dbObjRef != null) {
+				// an entity with the id already exists in database
+				// push this entity to the operand stack
+				mem.getDB().addRequiredEntity(dbObjRef, vm.getSolverManager().getConstraintLevel());
+				frame.getOperandStack().push(dbObjRef);
+			} else {
+				// there exists no entity with the given id
+				// create a new object-reference-variable as
+				// a result of the #find method
+				ClassFile classFile = null;
+				try {
+					classFile = vm.getClassLoader().getClassAsClassFile(className);
+				} catch(ClassFileException e) {
+					throw new SpecialMethodInvokeException("Could not load required class file: " + className);
+				}
 				
-		frame.getOperandStack().push(entityFindVar);
+				String entityFindName = "FindResult@" + frame.getMethod().getName() + ":" + frame.getPc();
+				ObjectrefVariable entityFindVar = new ObjectrefVariable(entityFindName, new InitializedClass(classFile, vm), vm);
+				
+				mem.getDB().addRequiredEntity(entityFindVar, vm.getSolverManager().getConstraintLevel());
+				frame.getOperandStack().push(entityFindVar);
+			}
+		} catch (SymbolicDatabaseException e) {
+			throw new SpecialMethodInvokeException("Error while generating find result object", e);
+		}
 	}
 	
 	private String getClassName(Object object) throws SpecialMethodInvokeException {
