86fa9334963b7603ce981df730d40b06a099f7e8
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-28 18:25:28 +0200

    add constants for JPA entity annotations

--- muggl-common/src/de/wwu/muggl/configuration/JavaEEConstants.java
+++ muggl-common/src/de/wwu/muggl/configuration/JavaEEConstants.java
@@ -18,15 +18,35 @@ public final class JavaEEConstants {
 	public static final String ANNOTATION_POST_CONSTRUCT = "Ljavax/annotation/PostConstruct;";
 	
 	/**
-	 * The Java String representation of the JPA annotation @javax.persistence.PersistenceContext.
-	 */
-	public static final String ANNOTATION_PERSISTENCE_ID = "Ljavax/persistence/Id;";
-	
-	/**
 	 * The Java String representation of the JPA annotation @javax.persistence.Entity.
 	 */
 	public static final String ANNOTATION_PERSISTENCE_ENTITY = "Ljavax/persistence/Entity;";
 	
+	// -------------------------------------------------------------
+	// JPA Entity Attributes
+	
+	/**
+	 * The Java String representation of the JPA annotation @javax.persistence.Id.
+	 */
+	public static final String ANNOTATION_PERSISTENCE_ID = "Ljavax/persistence/Id;";
+	
+	/**
+	 * The Java String representation of the JPA annotation javax.validation.constraints.NotNull.
+	 */
+	public static final String ANNOTATION_VALIDATION_CONSTRAINTS_NOT_NULL = "Ljavax/validation/constraints/NotNull;";
+	
+	/**
+	 * The Java String representation of the JPA annotation javax.validation.constraints.Min.
+	 */
+	public static final String ANNOTATION_VALIDATION_CONSTRAINTS_MIN = "Ljavax/validation/constraints/Min;";
+	
+	/**
+	 * The Java String representation of the JPA annotation javax.validation.constraints.Min.
+	 */
+	public static final String ANNOTATION_VALIDATION_CONSTRAINTS_MAX = "Ljavax/validation/constraints/Max;";
+	
+	// -------------------------------------------------------------
+		
 	/**
 	 * The fully qualified name of the class of the JPA EntityManager.
 	 */
