eab79b4dc208b794534a5240b76da22b75d26752
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-28 18:43:10 +0200

    add pre execution required data for JPA test cases

--- muggl-core/src/de/wwu/muggl/symbolic/testCases/JavaEETestCaseSolution.java
+++ muggl-core/src/de/wwu/muggl/symbolic/testCases/JavaEETestCaseSolution.java
@@ -1,11 +1,17 @@
 package de.wwu.muggl.symbolic.testCases;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import de.wwu.muggl.javaee.jpa.SymbolicDatabase;
 import de.wwu.muggl.javaee.testcase.obj.ObjectBuilder;
+import de.wwu.muggl.javaee.testcase.obj.impl.EntityObjectBuilder;
 import de.wwu.muggl.solvers.Solution;
+import de.wwu.muggl.solvers.expressions.NumericConstant;
+import de.wwu.muggl.solvers.expressions.NumericVariable;
+import de.wwu.muggl.symbolic.var.ObjectrefVariable;
+import de.wwu.muggl.vm.classfile.ClassFile;
 import de.wwu.muggl.vm.classfile.structures.Method;
 import de.wwu.muggl.vm.initialization.Objectref;
 
@@ -41,28 +47,57 @@ public class JavaEETestCaseSolution extends TestCaseSolution {
 	}
 
 	private void init() {
-		this.objectBuilder = new ObjectBuilder(solution);
+		this.objectBuilder = new EntityObjectBuilder(solution);
 		this.dbPre = new StringBuilder();
 		buildPreExecutionRequiredDatabase();
 	}
 	
 	private void buildPreExecutionRequiredDatabase() {
 		dbPre.append("\t\t// pre-execution required data\n");
-		boolean emptyPreReqData = true; // flag to indicate that no pre execution required data exists
+		boolean preExeDataExists = true; // flag to indicate that no pre execution required data exists
 		Map<String, Set<Objectref>> data = symbolicDatabase.getPreExecutionRequiredData();
 		for(String entityName : data.keySet()) {
 			for(Objectref objRef : data.get(entityName)) {
-				dbPre.append("\t\t// generate data for: " + objRef);
-				emptyPreReqData = false;
+				preExeDataExists |= generateData(objRef);
 			}
 		}
-		if(emptyPreReqData) {
+		if(!preExeDataExists) {
 			dbPre.append("\t\t// no pre-execution data required for this test case");
 		}
 		dbPre.append("\n");
 	}
 
+	protected boolean generateData(Objectref objRef) {
+		dbPre.append("\t\t// generate data for: " + objRef+"\n");
+		if(objRef instanceof ObjectrefVariable) {
+			ObjectrefVariable objRefVar = (ObjectrefVariable) objRef;
+			NumericVariable nv = objRefVar.getIsNullVariable();
+			NumericConstant nc = (NumericConstant)this.solution.getValue(nv);
+			if(nc != null && nc.getIntValue() == 0) {
+				objectBuilder.getObjectName(objRef, dbPre);
+//				generateNotNullObjectReference(objRefVar);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+//	private void generateNotNullObjectReference(Objectref objRef) {
+//		ClassFile classFile = objRef.getInitializedClass().getClassFile();
+//		String fullClassName = classFile.getName();
+//		this.requiredPackages.add(fullClassName);
+//		
+//		String className = classFile.getClassName();
+//		String objName = "foo";
+//		dbPre.append("\t\t"+className+" "+objName + " = new "+className+"();\n");
+//		
+//		
+//		
+//	}
+
 	public String getPreExecutionRequiredDatabaseString() {
 		return this.dbPre.toString();
 	}
+	
+	
 }
