89900e84d741b8c9cf796dfdd4d1db7e44c3b8f6
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-06-28 18:27:17 +0200

    set also the id field to object reference and add static constraints

--- muggl-core/src/de/wwu/muggl/javaee/invoke/impl/MugglEntityManagerFind.java
+++ muggl-core/src/de/wwu/muggl/javaee/invoke/impl/MugglEntityManagerFind.java
@@ -3,16 +3,23 @@ package de.wwu.muggl.javaee.invoke.impl;
 import de.wwu.muggl.javaee.invoke.SpecialMethodInvocation;
 import de.wwu.muggl.javaee.invoke.SpecialMethodInvokeException;
 import de.wwu.muggl.javaee.invoke.SpecialMethodUtil;
+import de.wwu.muggl.javaee.jpa.JPAEntityClassAnalyzer;
 import de.wwu.muggl.javaee.jpa.MugglEntityManager;
 import de.wwu.muggl.javaee.jpa.SymbolicDatabaseException;
-import de.wwu.muggl.solvers.expressions.IntConstant;
+import de.wwu.muggl.javaee.jpa.cstr.EntityConstraintException;
+import de.wwu.muggl.javaee.jpa.cstr.StaticConstraintManager;
+import de.wwu.muggl.solvers.exceptions.SolverUnableToDecideException;
+import de.wwu.muggl.solvers.exceptions.TimeoutException;
+import de.wwu.muggl.solvers.expressions.Expression;
+import de.wwu.muggl.solvers.expressions.NumericConstant;
+import de.wwu.muggl.solvers.expressions.NumericEqual;
+import de.wwu.muggl.solvers.expressions.NumericVariable;
 import de.wwu.muggl.symbolic.var.ObjectrefVariable;
 import de.wwu.muggl.vm.Frame;
 import de.wwu.muggl.vm.classfile.ClassFile;
 import de.wwu.muggl.vm.classfile.ClassFileException;
 import de.wwu.muggl.vm.classfile.structures.Field;
 import de.wwu.muggl.vm.impl.symbolic.SymbolicVirtualMachine;
-import de.wwu.muggl.vm.initialization.Arrayref;
 import de.wwu.muggl.vm.initialization.InitializedClass;
 import de.wwu.muggl.vm.initialization.Objectref;
 
@@ -60,13 +67,33 @@ public class MugglEntityManagerFind implements SpecialMethodInvocation {
 					throw new SpecialMethodInvokeException("Could not load required class file: " + className);
 				}
 				
+				// generate a new object reference variable of the given type, i.e. of the given class in parameter
 				String entityFindName = "FindResult@" + frame.getMethod().getName() + ":" + frame.getPc();
 				ObjectrefVariable entityFindVar = new ObjectrefVariable(entityFindName, new InitializedClass(classFile, vm), vm);
+				
+				// set the given id value from the parameter as its id value
+				Field idField = JPAEntityClassAnalyzer.getInst().getIdField(className, vm);
+				entityFindVar.putField(idField, idValue);
+				
+				// check if the idValue is an object reference variable, in that case -> it cannot be null
+				if(idValue instanceof ObjectrefVariable) {
+					ObjectrefVariable objRefVar = (ObjectrefVariable)idValue;
+					NumericVariable nv = objRefVar.getIsNullVariable();
+					vm.getSolverManager().addConstraint(NumericEqual.newInstance(nv, NumericConstant.getZero(Expression.BOOLEAN)));
+				}
+				
+				// add static entity constraints, e.g., minimum and maximum values, etc.
+				StaticConstraintManager.getInst().setStaticConstraints(entityFindVar, vm);
+				
+				// check if there is still a solution possible
+				if(!vm.getSolverManager().hasSolution()) {
+					throw new SpecialMethodInvokeException("Static constraints cannot be added to object reference ["+entityFindVar+"] and cause an error");
+				}				
 				
 				mem.getDB().addRequiredEntity(entityFindVar, vm.getSolverManager().getConstraintLevel());
 				frame.getOperandStack().push(entityFindVar);
 			}
-		} catch (SymbolicDatabaseException e) {
+		} catch (SymbolicDatabaseException | EntityConstraintException | TimeoutException | SolverUnableToDecideException e) {
 			throw new SpecialMethodInvokeException("Error while generating find result object", e);
 		}
 	}
