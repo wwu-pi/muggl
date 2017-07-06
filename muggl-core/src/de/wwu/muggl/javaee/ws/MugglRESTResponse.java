5fc4fd90956ab3fd19df21b340f2ee5c5c83fdee
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-07-06 16:55:01 +0200

    add Muggl REST response as special object-reference

---
+++ muggl-core/src/de/wwu/muggl/javaee/ws/MugglRESTResponse.java
@@ -0,0 +1,13 @@
+package de.wwu.muggl.javaee.ws;
+
+import de.wwu.muggl.symbolic.var.ObjectrefVariable;
+import de.wwu.muggl.vm.impl.symbolic.SymbolicVirtualMachine;
+import de.wwu.muggl.vm.initialization.InitializedClass;
+
+public class MugglRESTResponse extends ObjectrefVariable {
+
+	public MugglRESTResponse(String name, InitializedClass staticReference, SymbolicVirtualMachine vm) {
+		super(name, staticReference, vm);
+	}
+
+}
