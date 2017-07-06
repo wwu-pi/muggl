108cd0cce6f271cf32c9cc42cc6ca8be3ea01916
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-07-06 16:49:43 +0200

    creating a symbolic REST resource

--- muggl-core/src/de/wwu/muggl/javaee/ws/MugglWsRsInvocationBuilder.java
+++ muggl-core/src/de/wwu/muggl/javaee/ws/MugglWsRsInvocationBuilder.java
@@ -3,6 +3,8 @@ package de.wwu.muggl.javaee.ws;
 import java.util.HashSet;
 import java.util.Set;
 
+import de.wwu.muggl.javaee.rest.RESTResource;
+import de.wwu.muggl.javaee.rest.RESTResourceManager;
 import de.wwu.muggl.vm.impl.symbolic.SymbolicVirtualMachine;
 
 public class MugglWsRsInvocationBuilder extends MugglWsRs {
@@ -13,6 +15,11 @@ public class MugglWsRsInvocationBuilder extends MugglWsRs {
 	protected MugglWsRsTarget target;
 	
 	/**
+	 * The REST resource that is built by this invocation builder.
+	 */
+	protected RESTResource restResource;
+	
+	/**
 	 * The accepted response types as a set of strings.
 	 */
 	protected Set<String> acceptedResponseTypes;
@@ -20,10 +27,24 @@ public class MugglWsRsInvocationBuilder extends MugglWsRs {
 	public MugglWsRsInvocationBuilder(SymbolicVirtualMachine vm, MugglWsRsTarget target) throws MugglWsRsException {
 		super("javax.ws.rs.client.Invocation$Builder", vm);
 		this.acceptedResponseTypes = new HashSet<>();
+		this.target = target;
+		buildRESTResource();
+	}
+
+	private void buildRESTResource() {
+		String endpoint = this.target.getTargetUrl();
+		this.restResource = new RESTResource(endpoint);
+		
+		int constraintLevel = this.vm.getSolverManager().getConstraintLevel();
+		RESTResourceManager.getInst().addRequiredResource(constraintLevel, this.restResource);
 	}
 
 	public void addAcceptedResponseType(String acceptedResponseType) {
 		this.acceptedResponseTypes.add(acceptedResponseType);
+	}
+	
+	public RESTResource getRESTResource() {
+		return this.restResource;
 	}
 
 }
