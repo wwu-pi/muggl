72e7dc048120f703866050680634b3c028943172
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-07-06 16:50:48 +0200

    add required REST resources to solution

--- muggl-core/src/de/wwu/muggl/symbolic/testCases/JavaEETestCaseSolution.java
+++ muggl-core/src/de/wwu/muggl/symbolic/testCases/JavaEETestCaseSolution.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Set;
 
 import de.wwu.muggl.javaee.jpa.SymbolicDatabase;
+import de.wwu.muggl.javaee.rest.RESTResource;
 import de.wwu.muggl.javaee.testcase.obj.ObjectBuilder;
 import de.wwu.muggl.javaee.testcase.obj.impl.EntityObjectBuilder;
 import de.wwu.muggl.solvers.Solution;
@@ -26,32 +27,49 @@ public class JavaEETestCaseSolution extends TestCaseSolution {
 	
 	protected SymbolicDatabase symbolicDatabase;
 	
+	protected Set<RESTResource> requiredRESTResources;
+	
 	// the string builder for the pre-execution required database state
 	protected StringBuilder dbPre;
 	
+	// the string builder for the required web services
+	protected StringBuilder wsReq;
+	
 	public JavaEETestCaseSolution(Method initialMethod, Solution solution, Object returnValue,
-			SymbolicDatabase symbolicDatabase, boolean throwsAnUncaughtException,
+			SymbolicDatabase symbolicDatabase, Set<RESTResource> requiredRESTResources,
+			boolean throwsAnUncaughtException,
 			Object[] variables, boolean[] dUCoverage, Map<Method, boolean[]> cFCoverageMapping) {
 		super(initialMethod, solution, returnValue, throwsAnUncaughtException, variables, dUCoverage, cFCoverageMapping);
 		this.symbolicDatabase = symbolicDatabase;
+		this.requiredRESTResources = requiredRESTResources;
 		init();
 	}
 
 	public JavaEETestCaseSolution(Method initialMethod, Solution solution, Object returnValue,
-			SymbolicDatabase symbolicDatabase, boolean throwsAnUncaughtException,
+			SymbolicDatabase symbolicDatabase, Set<RESTResource> requiredRESTResources,
+			boolean throwsAnUncaughtException,
 			Object[] variables, boolean[] dUCoverage, Map<Method, boolean[]> cFCoverageMapping,
 			TestCaseSolution latestSolutionFound) {
 		super(initialMethod, solution, returnValue, throwsAnUncaughtException, variables, dUCoverage, cFCoverageMapping, latestSolutionFound);
 		this.symbolicDatabase = symbolicDatabase;
+		this.requiredRESTResources = requiredRESTResources;
 		init();
 	}
 
 	private void init() {
 		this.objectBuilder = new EntityObjectBuilder(solution);
 		this.dbPre = new StringBuilder();
+		this.wsReq = new StringBuilder();
 		buildPreExecutionRequiredDatabase();
+		buildRequiredWebServices();
 	}
 	
+	private void buildRequiredWebServices() {
+		for(RESTResource restResource : this.requiredRESTResources) {
+			this.wsReq.append("required resource: " + restResource + "\n");
+		}
+	}
+
 	private void buildPreExecutionRequiredDatabase() {
 		dbPre.append("\t\t// pre-execution required data\n");
 		boolean preExeDataExists = false; // flag to indicate that no pre execution required data exists
@@ -84,5 +102,9 @@ public class JavaEETestCaseSolution extends TestCaseSolution {
 
 	public String getPreExecutionRequiredDatabaseString() {
 		return this.dbPre.toString();
+	}
+	
+	public String getRequiredWebServicesString() {
+		return this.wsReq.toString();
 	}
 }
