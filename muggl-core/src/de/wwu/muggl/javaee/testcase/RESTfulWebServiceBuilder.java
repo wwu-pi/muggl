b86ec0b1469f54ed20617c97cc5fbf87d7203133
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-07-07 14:44:51 +0200

    add empty REST builder

---
+++ muggl-core/src/de/wwu/muggl/javaee/testcase/RESTfulWebServiceBuilder.java
@@ -0,0 +1,17 @@
+package de.wwu.muggl.javaee.testcase;
+
+import de.wwu.muggl.javaee.rest.RESTResource;
+
+/**
+ * This class builds a RESTful web service.
+ * 
+ * @author Andreas Fuchs
+ */
+public class RESTfulWebServiceBuilder {
+
+	protected RESTResource resource;
+	
+	public RESTfulWebServiceBuilder(RESTResource resource) {
+		
+	}
+}
