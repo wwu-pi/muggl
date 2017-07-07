35175318d550e1ca0e3376d36026d5ba2aabc47f
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-07-07 16:16:29 +0200

    add initial query result list as object reference variable

---
+++ muggl-core/src/de/wwu/muggl/javaee/jpa/SymbolicQueryResultList.java
@@ -0,0 +1,31 @@
+package de.wwu.muggl.javaee.jpa;
+
+import de.wwu.muggl.solvers.expressions.Expression;
+import de.wwu.muggl.solvers.expressions.NumericConstant;
+import de.wwu.muggl.solvers.expressions.NumericEqual;
+import de.wwu.muggl.symbolic.var.ObjectrefVariable;
+import de.wwu.muggl.vm.classfile.ClassFile;
+import de.wwu.muggl.vm.classfile.ClassFileException;
+import de.wwu.muggl.vm.impl.symbolic.SymbolicVirtualMachine;
+import de.wwu.muggl.vm.initialization.InitializedClass;
+
+public class SymbolicQueryResultList extends ObjectrefVariable {
+
+	public SymbolicQueryResultList(String name, SymbolicVirtualMachine vm) {
+		super(name, getListReference(vm), vm);
+		
+		// set this list to be NOT null
+		vm.getSolverManager().addConstraint(NumericEqual.newInstance(this.isNull, NumericConstant.getZero(Expression.BOOLEAN)));
+	}
+
+	private static InitializedClass getListReference(SymbolicVirtualMachine vm) {
+		try {
+			ClassFile listClassFile = vm.getClassLoader().getClassAsClassFile(java.util.ArrayList.class.getName());
+			return vm.getAnObjectref(listClassFile).getInitializedClass();
+		} catch (ClassFileException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}
