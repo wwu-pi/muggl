3a6f6dbf61ccaab91ba1e5fb1f6209cf98d54f09
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-07-07 16:44:39 +0200

    symoblic array list wrapper

---
+++ muggl-core/src/de/wwu/muggl/javaee/jpa/SymbolicArrayList.java
@@ -0,0 +1,17 @@
+package de.wwu.muggl.javaee.jpa;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class SymbolicArrayList<E> extends ArrayList<E> {
+
+	private static final long serialVersionUID = 1L;
+ 
+	
+	
+	@Override
+	public Iterator<E> iterator() {
+		
+		return super.iterator();
+	}
+}
