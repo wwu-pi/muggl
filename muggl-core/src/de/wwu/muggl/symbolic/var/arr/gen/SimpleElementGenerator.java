c8da0463bba7acff8eca286d68b89354cfcdc327
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-08-13 14:19:45 +0200

    generated object cannot be null

--- muggl-core/src/de/wwu/muggl/symbolic/var/arr/gen/SimpleElementGenerator.java
+++ muggl-core/src/de/wwu/muggl/symbolic/var/arr/gen/SimpleElementGenerator.java
@@ -1,5 +1,8 @@
 package de.wwu.muggl.symbolic.var.arr.gen;
 
+import de.wwu.muggl.solvers.expressions.Expression;
+import de.wwu.muggl.solvers.expressions.NumericConstant;
+import de.wwu.muggl.solvers.expressions.NumericEqual;
 import de.wwu.muggl.symbolic.var.ObjectrefVariable;
 import de.wwu.muggl.vm.impl.symbolic.SymbolicVirtualMachine;
 import de.wwu.muggl.vm.initialization.InitializedClass;
@@ -23,13 +26,16 @@ public class SimpleElementGenerator implements ArrayElementGenerator {
 	
 	public SimpleElementGenerator(String parentName, InitializedClass type, SymbolicVirtualMachine vm) {
 		this.count = 0;
+		this.parentName = parentName;
 		this.vm = vm;
 		this.type = type;
 	}
 	
 	@Override
 	public ObjectrefVariable generateElement() {
-		return new ObjectrefVariable(parentName+".genElement_"+count++, type, vm);
+		ObjectrefVariable ele = new ObjectrefVariable(parentName+".genElement_"+count++, type, vm);
+		vm.getSolverManager().addConstraint(NumericEqual.newInstance(ele.getIsNullVariable(), NumericConstant.getZero(Expression.BOOLEAN)));
+		return ele;
 	}
 
 }
