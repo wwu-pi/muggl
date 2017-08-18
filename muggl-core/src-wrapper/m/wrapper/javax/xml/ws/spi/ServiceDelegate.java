db4a6e79f6226a3fb887a7792f3867d29466d9b9
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-08-18 10:12:37 +0200

    add wrapper folder

---
+++ muggl-core/src-wrapper/m/wrapper/javax/xml/ws/spi/ServiceDelegate.java
@@ -0,0 +1,9 @@
+package m.wrapper.javax.xml.ws.spi;
+
+public abstract class ServiceDelegate extends javax.xml.ws.spi.ServiceDelegate {
+
+	protected java.net.URL wsdlDocumentLocation;
+	protected javax.xml.namespace.QName serviceName;
+	protected java.lang.Class<?> serviceClass;
+	
+}
