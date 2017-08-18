444007c9fd04ca071aef3fd1beaa7285e1ff70e7
a_fuch05 <andreas.fuchs@wi.uni-muenster.de>
2017-08-18 18:07:30 +0200

    new web servcie manager

---
+++ muggl-core/src/de/wwu/muggl/javaee/jaxws/WebServiceManager.java
@@ -0,0 +1,37 @@
+package de.wwu.muggl.javaee.jaxws;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import de.wwu.muggl.javaee.jaxws.sym.WebService;
+
+public class WebServiceManager {
+
+	private static WebServiceManager inst;
+	
+	protected Set<WebService> webSerivceSet;
+	
+	private WebServiceManager() {
+		this.webSerivceSet = new HashSet<>();
+	}
+	
+	public synchronized static WebServiceManager getInstance() {
+		if(inst == null) {
+			inst = new WebServiceManager();
+		}
+		return inst;
+	}
+
+	public WebService getWebService(String serviceName, String targetNamespace, String wsdlLocation) {
+		for(WebService webService : this.webSerivceSet) {
+			if(webService.getName().equals(serviceName)
+				&& webService.getTargetNamespace().equals(targetNamespace)
+				&& webService.getWsdlLocation().equals(wsdlLocation)) {
+				return webService;
+			}
+		}
+		WebService webService = new WebService(serviceName, targetNamespace, wsdlLocation);
+		this.webSerivceSet.add(webService);
+		return webService;
+	}
+}
