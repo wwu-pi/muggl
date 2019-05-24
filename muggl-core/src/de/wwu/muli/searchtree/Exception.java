959b4e36649e297d5b0eb1e4733d0a765e2de234
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2019-05-24 16:27:50 +0200

    Record exception leaves

--- muggl-core/src/de/wwu/muli/searchtree/Exception.java
+++ muggl-core/src/de/wwu/muli/searchtree/Exception.java
@@ -1,5 +1,13 @@
 package de.wwu.muli.searchtree;
 
 public class Exception extends ST {
-    public Throwable exception;
+    public final Object exception;
+
+    /**
+     * Represent an exception solution leaf.
+     * @param exception Type is object since exceptions returned from symbolic execution could be of type `Objectref'.
+     */
+    public Exception(Object exception) {
+        this.exception = exception;
+    }
 }
