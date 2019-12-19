b4f4f399ba30028d0b04a8d436f8047e54e8baa5
Jan Dageförde <jan.dagefoerde@ercis.uni-muenster.de>
2019-12-19 13:19:04 +0100

    remove obsolete package info

--- muggl-core/src/de/wwu/muggl/vm/impl/logic/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Provides the virtual machine implementation for logic computation. It inherits functionality from
- * the symbolic virtual machine but overrides some of it.
- * 
- * @author Tim Majchrzak
- */
-package de.wwu.muggl.vm.impl.logic;
