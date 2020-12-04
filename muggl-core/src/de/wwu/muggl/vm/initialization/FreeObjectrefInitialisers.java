c48a742f397aa54c996b5e06e18aa83d5bfe602e
Hendrik Winkelmann <hendrik.winkelmann@uni-muenster.de>
2020-12-04 11:05:35 +0100

    Added initialization of FreeArrayrefs

--- muggl-core/src/de/wwu/muggl/vm/initialization/FreeObjectrefInitialisers.java
+++ muggl-core/src/de/wwu/muggl/vm/initialization/FreeObjectrefInitialisers.java
@@ -3,14 +3,36 @@ package de.wwu.muggl.vm.initialization;
 import de.wwu.muggl.solvers.expressions.BooleanVariable;
 import de.wwu.muggl.solvers.expressions.Expression;
 import de.wwu.muggl.solvers.expressions.NumericVariable;
+import de.wwu.muggl.solvers.expressions.Term;
 import de.wwu.muggl.vm.SearchingVM;
 import de.wwu.muggl.vm.VirtualMachine;
 import de.wwu.muggl.vm.classfile.ClassFile;
+import de.wwu.muggl.vm.classfile.ClassFileException;
 import de.wwu.muggl.vm.classfile.structures.Field;
 import de.wwu.muggl.vm.exceptions.VmRuntimeException;
 import de.wwu.muggl.vm.execution.ExecutionException;
 
 public class FreeObjectrefInitialisers {
+
+    public static Object createFreeObjectWithFreeFields(SearchingVM vm, ClassFile classFile, String name) {
+        // TODO Very easily this currently leads to an infinite loop.
+        // Get and check the initialized class.
+        InitializedClass initializedClass = classFile.getTheInitializedClass(vm);
+        // Get the object reference.
+        FreeObjectref objectref = initializedClass.getANewFreeObject();
+        Field[] fields = classFile.getFields();
+        for (Field f : fields) {
+            if (f.getName().contains("$this0")) {
+                continue;
+            }
+            Object fieldObjectref = createRepresentationForFreeVariableOrField(vm, f.getClassFile(), f.getDescriptor(), name + f.getName());
+            objectref.putField(f, fieldObjectref);
+        }
+        return objectref;
+    }
+
+
+
     public static Object createRepresentationForFreeVariableOrField(SearchingVM vm, ClassFile fromClass, String type, String name) {
         // Convert string type to expression type.
         Expression.Type expressionType = null;
@@ -52,12 +74,11 @@ public class FreeObjectrefInitialisers {
         }
 
         Object freeVariableRepresentation;
-
         // Find the correct type and create an appropriate representation.
         if (expressionType == Expression.Type.OBJECT) {
             freeVariableRepresentation = FreeObjectrefInitialisers.createRepresentationForFreeObject(vm, fromClass, type);
         } else if (expressionType == Expression.Type.ARRAY) {
-            throw new IllegalStateException("Free variables of array types are not supported yet.");
+            freeVariableRepresentation = createRepresentationForFreeArray(vm, fromClass, type, name);
         } else if (expressionType == Expression.Type.BOOLEAN) {
             freeVariableRepresentation = new BooleanVariable(name);
         } else {
@@ -66,15 +87,8 @@ public class FreeObjectrefInitialisers {
         return freeVariableRepresentation;
     }
 
-    private static Object createRepresentationForFreeObject(SearchingVM vm, ClassFile fromClass, String type) {
-	    // Resolve class of target type.
-        ClassFile classFile;
-        try {
-            classFile = vm.resolveClassAsClassFile(fromClass, type);
-        } catch (VmRuntimeException | ExecutionException e) {
-            throw new IllegalStateException(e);
-        }
-
+    private static FreeObjectref createRepresentationForFreeObject(SearchingVM vm, ClassFile fromClass, String type) {
+        ClassFile classFile = resolveOrThrowException(vm, fromClass, type);
         // Get an uninitialised(!) Objectref.
         FreeObjectref anObjectref = vm.getAFreeObjectref(classFile);
 
@@ -92,5 +106,82 @@ public class FreeObjectrefInitialisers {
             anObjectref.putField(field, representation);
         }
         return anObjectref;
+    }
+
+    private static FreeArrayref createRepresentationForFreeArray(
+            SearchingVM vm,
+            ClassFile fromClass,
+            String type,
+            String name) {
+        if (!type.startsWith("[")) {
+            throw new IllegalArgumentException("Type is not an array.");
+        }
+        if (type.startsWith("[[")) {
+            throw new UnsupportedOperationException("Nested arrays not yet regarded.");
+        }
+        boolean isObjectArray = type.contains("[L");
+        ClassFile classFile;
+        ReferenceValue referenceValue;
+        // For primitive arrays
+        InitializedClass initializedClass = null;
+        if (isObjectArray) {
+            classFile = resolveOrThrowException(vm, fromClass, type.replaceAll("\\[", ""));
+            referenceValue = vm.getAFreeObjectref(classFile);
+        } else {
+            if (type.contains("[F")) { // float
+                classFile = resolveOrThrowException(vm, fromClass, Float.class.getName());
+            } else if (type.contains("[D")) { // double
+                classFile = resolveOrThrowException(vm, fromClass, Double.class.getName());
+            } else if (type.contains("[Z")) { // boolean
+                classFile = resolveOrThrowException(vm, fromClass, Boolean.class.getName());
+            } else if (type.contains("[B")) { // byte
+                classFile = resolveOrThrowException(vm, fromClass, Byte.class.getName());
+            } else if (type.contains("[S")) { // short
+                classFile = resolveOrThrowException(vm, fromClass, Short.class.getName());
+            } else if (type.contains("[I")) { // int
+                classFile = resolveOrThrowException(vm, fromClass, Integer.class.getName());
+            } else if (type.contains("[J")) { // long
+                classFile = resolveOrThrowException(vm, fromClass, Long.class.getName());
+            } else {
+                throw new IllegalStateException("Array type is not supported: " + type);
+            }
+            referenceValue = vm.getAnObjectref(classFile);
+            initializedClass = classFile.getTheInitializedClass(vm);
+
+        }
+
+        FreeArrayref result = new FreeArrayref(
+                name,
+                referenceValue,
+                new NumericVariable(name+".length", Expression.Type.INT.toByte()));
+        if (!isObjectArray) {
+            result.setRepresentedTypeAsAPrimitiveWrapper(initializedClass);
+        }
+
+        result = createNestingIfNeeded(result, type);
+
+        return result;
+    }
+
+    private static FreeArrayref createNestingIfNeeded(FreeArrayref innerArrayref, String type) {
+//        int nestingCount = 0;
+//        for (int i = 0; i < type.length(); i++) {
+//            if (type.charAt(i) == '[') {
+//                nestingCount++;
+//            } else {
+//                break;
+//            }
+//        }
+        // TODO Nested arrayrefs
+        return innerArrayref;
+    }
+
+    private static ClassFile resolveOrThrowException(SearchingVM vm, ClassFile fromClass, String type) throws IllegalStateException {
+        // Resolve class of target type.
+        try {
+            return vm.resolveClassAsClassFile(fromClass, type);
+        } catch (VmRuntimeException | ExecutionException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
